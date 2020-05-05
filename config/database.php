1c8bc9ef064d9f363cfc6386ea2c32b9cee301b2
viandwi24 <fiandwi0424@gmail.com>
2020-05-06 00:06:40 +0700

    Heroku fix database config

--- config/database.php
+++ config/database.php
@@ -72,7 +72,7 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            // 'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             // 'database' => env('DB_DATABASE', 'forge'),
             // 'username' => env('DB_USERNAME', 'forge'),
@@ -81,7 +81,7 @@ return [
             'database' => $database,
             'username' => $username,
             'password' => $password,
-            
+
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
