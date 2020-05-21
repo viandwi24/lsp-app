93a5b6c15ca5a47e02a777e2115299897a220859
viandwi24 <fiandwi0424@gmail.com>
2020-05-21 23:28:50 +0700

    fix models frmak01

--- app/Models/FrMak01.php
+++ app/Models/FrMak01.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class FrMak1 extends Model
+class FrMak01 extends Model
 {
     protected $table = 'fr_mak_01';
     protected $guarded = [];
