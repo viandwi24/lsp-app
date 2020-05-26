a8f1e2efc52f284bb8bf0c74db8483eebfd1f058
viandwi24 <fiandwi0424@gmail.com>
2020-05-27 01:24:57 +0700

    update db

---
+++ database/migrations/2020_05_26_165952_create_asesmen_frai01_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Query\Expression;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAsesmenFrai01Table extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('asesmen_frai01', function (Blueprint $table) {
+            $table->id();
+
+            $table->bigInteger('asesmen_id')->unsigned();
+            $table->foreign('asesmen_id')->references('id')->on('asesmen')->onUpdate('cascade')->onDelete('cascade');
+
+            $table->json('data')->default(new Expression('(JSON_ARRAY())'));
+            $table->enum('kinerja', ['memuaskan', 'tidak_memuaskan'])->default('memuaskan');
+            $table->longText('catatan')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('asesmen_frai01');
+    }
+}
