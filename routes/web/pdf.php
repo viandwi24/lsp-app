ec7b4b710f6c594a349c88398dae059144c2525b
viandwi24 <fiandwi0424@gmail.com>
2020-06-11 07:49:41 +0700

    update pdf

--- routes/web/pdf.php
+++ routes/web/pdf.php
@@ -6,5 +6,7 @@ Route::group([
     "prefix" => "pdf",
     "as" => "pdf."
 ], function () {
-    Route::get('frapl01/{id}', "PdfController@frapl01")->name("frapl01");
+    Route::get('frmak01/{id}', "PdfController@frmak01")->name("frmak01");
+    Route::get('frai01/{id}', "PdfController@frai01")->name("frai01");
+    Route::get('frai02/{id}', "PdfController@frai02")->name("frai02");
 });
