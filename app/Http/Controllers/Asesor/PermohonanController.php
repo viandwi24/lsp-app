8716c06b964cd177627f84d5ef25228c6480a338
viandwi24 <fiandwi0424@gmail.com>
2020-07-06 23:47:33 +0700

    added bulk approve in asesor permohonan and status

--- app/Http/Controllers/Asesor/PermohonanController.php
+++ app/Http/Controllers/Asesor/PermohonanController.php
@@ -19,20 +19,29 @@ class PermohonanController extends Controller
             $permohonan = PermohonanAsesiAsesor::with(['permohonan', 'permohonan.skema' => function ($query) { return $query->select('id', 'judul', 'kode'); }, 'permohonan.asesi' => function ($query) { return $query->select('id', 'nama'); }])
                 ->where('asesor_id', auth()->user()->id)->get();
             return DataTables::of($permohonan)
+                ->addColumn('status', function (PermohonanAsesiAsesor $permohonan) {
+                    return ($permohonan->status == "Disetujui") 
+                        ? '<span class="badge badge-success">'.$permohonan->status.'</span>'
+                        : '<span class="badge badge-danger">'.$permohonan->status.'</span>';
+                })
+                ->addColumn('created_at', function (PermohonanAsesiAsesor $permohonan) {
+                    return $permohonan->created_at->format("d-m-Y");
+                })
                 ->addColumn('action', function (PermohonanAsesiAsesor $permohonan) {
                     $result = '<div>';
                     if ($permohonan->approved_at == null)   
                     {
-                        $result .= '<a href="'. route('asesor.permohonan.edit', [$permohonan->id]) .'?setujui" class="btn btn-sm btn-success"><i class="ft-check"></i></a>';
+                        $result .= '<a href="'. route('asesor.permohonan.edit', [$permohonan->id]) .'?setujui" class="btn btn-sm btn-success"><i class="ft-check"></i> Setujui</a>';
                     } else {
                         $result .= '<form method="post" action="'.route('asesor.permohonan.update', [$permohonan->id]).'?batal">' . csrf_field() . method_field('put');
-                        $result .= '<button class="btn btn-sm btn-danger"><i class="ft-x"></i></button>';
+                        $result .= '<button class="btn btn-sm btn-danger"><i class="ft-x"></i> Batal</button>';
                         $result .= '</form>';
                     }
                     $result .= '<div>';
                     return $result;
 
                 })
+                ->rawColumns(['status', 'action'])
                 ->make();
         }
 
@@ -48,33 +57,43 @@ class PermohonanController extends Controller
     }
 
     // 
-    public function update(PermohonanAsesiAsesor $permohonanAsesiAsesor)
+    public function update($id)
     {
-        if (isset($_GET['setujui']) && $permohonanAsesiAsesor->approved_at == null)
+        $ids = \explode(',', $id);
+        $permohonanAsesiAsesor = PermohonanAsesiAsesor::find($ids);
+        $destroy = $permohonanAsesiAsesor->each(function ($permohonanAsesiAsesor, $key) {
+            if (isset($_GET['setujui']) && $permohonanAsesiAsesor->approved_at == null)
+            {
+                $permohonan = Permohonan::findOrFail($permohonanAsesiAsesor->permohonan->id);
+                DB::transaction(function () use ($permohonanAsesiAsesor, $permohonan) {
+                    $permohonanAsesiAsesor->update(['approved_at' => Carbon::now()]);
+                    $permohonan->asesmen()->updateOrCreate([
+                        'asesor_id' => $permohonanAsesiAsesor->asesor_id,
+                        'asesi_id' => $permohonan->asesi_id,
+                        'skema_id' => $permohonan->skema_id,
+                        'jadwal_id' => $permohonanAsesiAsesor->jadwal_id,
+                        'tuk_id' => $permohonanAsesiAsesor->tuk_id,
+                    ]);
+                });
+            } else if (isset($_GET['batal']) && $permohonanAsesiAsesor->approved_at != null) {
+
+                // id
+                $permohonan = Permohonan::findOrFail($permohonanAsesiAsesor->permohonan->id);
+                DB::transaction(function () use ($permohonanAsesiAsesor, $permohonan) {
+                    $permohonanAsesiAsesor->update(['approved_at' => null]);
+                    $permohonan->asesmen()->delete();
+                });
+            }
+        });
+        
+        if (isset($_GET['setujui']))
         {
-            $permohonan = Permohonan::findOrFail($permohonanAsesiAsesor->permohonan->id);
-            DB::transaction(function () use ($permohonanAsesiAsesor, $permohonan) {
-                $permohonanAsesiAsesor->update(['approved_at' => Carbon::now()]);
-                $permohonan->asesmen()->updateOrCreate([
-                    'asesor_id' => $permohonanAsesiAsesor->asesor_id,
-                    'asesi_id' => $permohonan->asesi_id,
-                    'skema_id' => $permohonan->skema_id,
-                    'jadwal_id' => $permohonanAsesiAsesor->jadwal_id,
-                    'tuk_id' => $permohonanAsesiAsesor->tuk_id,
-                ]);
-            });
             return redirect()->route('asesor.permohonan.index')
                 ->with('alert', ['type' => 'success', 'title' => 'Sukses', 'text' => 'Permohonan Disetujui.']);
-        } else if (isset($_GET['batal']) && $permohonanAsesiAsesor->approved_at != null) {
-
-            // id
-            $permohonan = Permohonan::findOrFail($permohonanAsesiAsesor->permohonan->id);
-            DB::transaction(function () use ($permohonanAsesiAsesor, $permohonan) {
-                $permohonanAsesiAsesor->update(['approved_at' => null]);
-                $permohonan->asesmen()->delete();
-            });
+        } else if (isset($_GET['batal']))
+        {
             return redirect()->route('asesor.permohonan.index')
-                ->with('alert', ['type' => 'success', 'title' => 'Sukses', 'text' => 'Permohonan Dibatalkan.']);
+            ->with('alert', ['type' => 'success', 'title' => 'Sukses', 'text' => 'Permohonan Dibatalkan.']);
         }
 
         return redirect()->back();
--- resources/views/pages/asesor/permohonan/index.blade.php
+++ resources/views/pages/asesor/permohonan/index.blade.php
@@ -18,6 +18,14 @@ $breadcrumb = [
 
                 <x-dashboard-card title="Permohonan" classBody="card-table">
                     <x-slot name="heading">
+                        <div class="dropdown" style="display: inline-block;">
+                            <button class="btn btn-sm btn-danger dropdown-toggle disable-on-bulk-check-null" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Aksi Untuk <span class="bulk_check_count"></span> Item
+                            </button>
+                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                <a class="dropdown-item" id="bulkApprove">Setujui</a>
+                            </div>
+                        </div>
                         <li><a id="reload"><i class="ft-rotate-cw"></i></a></li>
                         <li><a data-action="expand"><i class="ft-maximize"></i></a></li>
                     </x-slot>
@@ -27,6 +35,12 @@ $breadcrumb = [
                         <table id="table" class="table table-striped table-bordered zero-configuration">
                             <thead>
                                 <tr>
+                                    <th width="5%">
+                                        <div class="custom-control custom-checkbox">
+                                            <input type="checkbox" id="bulk_check_selectall" class="custom-control-input">
+                                            <label class="custom-control-label" for="bulk_check_selectall"></label>
+                                        </div>
+                                    </th>
                                     <th width="8%">#</th>
                                     <th>Skema</th>
                                     <th>Asesi</th>
@@ -50,6 +64,7 @@ $breadcrumb = [
 
 @push('js-library')
     <script src="{{ assets('vendors/js/tables/datatable/datatables.min.js') }}" type="text/javascript"></script>
+    <script src="{{ assets('js/scripts/bulk-datatables.js') }}"></script>
 @endpush
 
 @push('js')
@@ -70,8 +85,17 @@ $breadcrumb = [
                         ajax: "{{ url()->route('asesor.permohonan.index') }}",
                         processing: true,
                         order: [[0, 'asc']],
-                        columnDefs: [ { orderable: false, targets: [5] }, ],
+                        columnDefs: [ { orderable: false, targets: [0, 6] }, ],
                         columns: [
+                            { 
+                                data: null,
+                                render: (data) => `
+                                    <div class="custom-control custom-checkbox">
+                                        <input type="checkbox" class="bulk custom-control-input" id="bulk_check` + data.id + `" name="bulk_check" value="` + data.id + `">
+                                        <label class="custom-control-label" for="bulk_check` + data.id + `"></label>
+                                    </div>
+                                `
+                            },
                             { render: (data, type, row, meta) => meta.row + meta.settings._iDisplayStart + 1 },
                             { data: 'permohonan.skema.judul' },
                             { data: 'permohonan.asesi.nama' },
@@ -85,6 +109,12 @@ $breadcrumb = [
             mounted() {
                 this.loadTable()
                 $('#reload').on('click', () => this.datatable.ajax.reload(null, false))
+                $('#bulkApprove').on('click', () => {
+                    var url = '{{ route("asesor.permohonan.index") }}/' + bulkSelectedItem + '?setujui'
+                    var form = $(`<form action="` + url + `" method="post"> @method('put') @csrf </form>`);
+                    $('body').append(form);
+                    form.submit();
+                })
             }
         })
     </script>
